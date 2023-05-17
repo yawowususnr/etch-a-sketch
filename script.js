5f543c7c093d44dd9982aba350c19166a269aed6
Yaw Owusu Snr <yawsnr@vt.edu>
2023-05-16 22:26:38 -0400

    allow for user to chose sketch size with button

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
 const container = document.querySelector('.container');
+const divs = document.querySelectorAll('.content');
+let size = 16;
+const btn = document.getElementById('btn');
 
 
-for (let i = 0; i < 256; i++) {
-    const j = document.createElement('div');
-    j.classList.add('content');
-    j.textContent = 'text';
 
-    container.appendChild(j)
+function draw (size) {
+    clearDiv();
+    for (let i = 0; i < size*size; i++) {
+        const j = document.createElement('div');
+        j.classList.add('content');
+        j.style.width = 100/(size) + "%" 
 
-    
+        container.appendChild(j)
+    }
 }
 
-const divs = document.querySelectorAll('.content')
 
-for (let i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('onmouseover', addHover(divs[i]))
+
+function paint() {
+    const divs = document.querySelectorAll('.content');
+    divs.forEach((test) => {
+        test.addEventListener(
+            "mouseover",
+            (event) => {
+            // highlight the mouseenter target
+            event.target.classList.add('hover')
+            }
+        );
+    });
 };
 
 
-function addHover(x) {
-    x.classList.add('hover')
+
+
+btn.onclick = () => {
+    const dim = prompt('how many');
+    console.log(dim);
+    draw(dim);
+    paint(divs);
 };
+
+function clearDiv() {
+    // access the div element and use the replaceChildren() method to clear the div content
+    const ele = document.querySelector('.container');
+    ele.replaceChildren();
+ }
+
+draw(16);
+paint(divs);
