f0641ac6da06e341f76627a43e6f8f67c78b364c
Yaw Owusu Snr <yawsnr@vt.edu>
2023-10-26 15:20:39 -0400

    add clear button

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # etch-a-sketch
+
 Project to create mini drawing tablet that responds to user Hover
--- script.js
+++ script.js
@@ -1,51 +1,48 @@
-const container = document.querySelector('.container');
-const divs = document.querySelectorAll('.content');
+const container = document.querySelector(".container");
+const divs = document.querySelectorAll(".content");
 let size = 16;
-const btn = document.getElementById('btn');
+const btn = document.getElementById("btn");
+const clearBtn = document.getElementById("btn-clear");
 
+function draw(sizes) {
+  size = sizes
+  for (let i = 0; i < sizes * sizes; i++) {
+    const j = document.createElement("div");
+    j.classList.add("content");
+    j.style.width = 100 / size + "%";
 
-
-function draw (size) {
-    clearDiv();
-    for (let i = 0; i < size*size; i++) {
-        const j = document.createElement('div');
-        j.classList.add('content');
-        j.style.width = 100/(size) + "%" 
-
-        container.appendChild(j)
-    }
+    container.appendChild(j);
+  }
 }
 
-
-
 function paint() {
-    const divs = document.querySelectorAll('.content');
-    divs.forEach((test) => {
-        test.addEventListener(
-            "mouseover",
-            (event) => {
-            // highlight the mouseenter target
-            event.target.classList.add('hover')
-            }
-        );
+  const divs = document.querySelectorAll(".content");
+  divs.forEach((test) => {
+    test.addEventListener("mouseover", (event) => {
+      // highlight the mouseenter target
+      event.target.classList.add("hover");
     });
-};
-
-
-
+  });
+}
 
 btn.onclick = () => {
-    const dim = prompt('how many');
-    console.log(dim);
-    draw(dim);
-    paint(divs);
+  const dim = prompt("how many");
+  clearDiv()
+  draw(dim);
+  paint(divs);
+};
+
+clearBtn.onclick = () => {
+  clearDiv()
+  draw(size);
+  paint(divs);
 };
 
 function clearDiv() {
-    // access the div element and use the replaceChildren() method to clear the div content
-    const ele = document.querySelector('.container');
-    ele.replaceChildren();
- }
+  // access the div element and use the replaceChildren() method to clear the div content
+  const ele = document.querySelector(".container");
+  ele.replaceChildren();
+}
 
 draw(16);
 paint(divs);
